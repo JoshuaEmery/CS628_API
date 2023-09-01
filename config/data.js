18ca0a751ac286213e7a11478c0f6dd0e82bf4f3
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2023-09-01 16:38:19 -0700

    Basic player data added

---
+++ config/data.js
@@ -0,0 +1,187 @@
+const playerData = [
+  {
+    first_name: "Randy",
+
+    last_name: "Arozarena",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/668227/headshot/67/current",
+
+    mlb_team: "Tampa Bay Rays",
+
+    player_rating: 524.092578,
+
+    max_hr_distance: "436",
+
+    avg_hr_distance: "399",
+
+    max_exit_velocity: "114.3",
+
+    avg_exit_velocity: "92.5",
+
+    player_id: "668227",
+  },
+
+  {
+    first_name: "Luis",
+
+    last_name: "Robert Jr",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/673357/headshot/67/current",
+
+    mlb_team: "Chicago White Sox",
+
+    player_rating: 499.9466249,
+
+    max_hr_distance: "450",
+
+    avg_hr_distance: "404",
+
+    max_exit_velocity: "113.6",
+
+    avg_exit_velocity: "88.4",
+
+    player_id: "673357",
+  },
+
+  {
+    first_name: "Adley",
+
+    last_name: "Rutschman",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/668939/headshot/67/current",
+
+    mlb_team: "Baltimore Orioles",
+
+    player_rating: 310.6554469,
+
+    max_hr_distance: "424",
+
+    avg_hr_distance: "398",
+
+    max_exit_velocity: "111.1",
+
+    avg_exit_velocity: "87.9",
+
+    player_id: "668939",
+  },
+
+  {
+    first_name: "Pete",
+
+    last_name: "Alonso",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/624413/headshot/67/current",
+
+    mlb_team: "New York Mets",
+
+    player_rating: 497.3539684,
+
+    max_hr_distance: "448",
+
+    avg_hr_distance: "404",
+
+    max_exit_velocity: "115.7",
+
+    avg_exit_velocity: "89.3",
+
+    player_id: "624413",
+  },
+
+  {
+    first_name: "Julio",
+
+    last_name: "Rodriguez",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/677594/headshot/67/current",
+
+    mlb_team: "Seattle Mariners",
+
+    player_rating: 696.5059549,
+
+    max_hr_distance: "454",
+
+    avg_hr_distance: "394",
+
+    max_exit_velocity: "116.7",
+
+    avg_exit_velocity: "92.9",
+
+    player_id: "677594",
+  },
+
+  {
+    first_name: "Mookie",
+
+    last_name: "Betts",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/605141/headshot/67/current",
+
+    mlb_team: "Los Angeles Dodgers",
+
+    player_rating: 124.6262701,
+
+    max_hr_distance: "427",
+
+    avg_hr_distance: "396",
+
+    max_exit_velocity: "107.9",
+
+    avg_exit_velocity: "90.3",
+
+    player_id: "605141",
+  },
+
+  {
+    first_name: "Vladimir",
+
+    last_name: "Guerrero Jr",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/665489/headshot/67/current",
+
+    mlb_team: "Toronto Blue Jays",
+
+    player_rating: 746.9198387,
+
+    max_hr_distance: "465",
+
+    avg_hr_distance: "409",
+
+    max_exit_velocity: "117.4",
+
+    avg_exit_velocity: "95",
+
+    player_id: "665489",
+  },
+
+  {
+    first_name: "Adolis",
+
+    last_name: "Garcia",
+
+    image_url:
+      "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/666969/headshot/67/current",
+
+    mlb_team: "Texas Rangers",
+
+    player_rating: 516.5686857,
+
+    max_hr_distance: "450",
+
+    avg_hr_distance: "398",
+
+    max_exit_velocity: "112.4",
+
+    avg_exit_velocity: "91",
+
+    player_id: "666969",
+  },
+];
+
+module.exports = playerData;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
+const playerData = require("./config/data");
 
 const port = process.env.PORT || 5000;
 
@@ -12,8 +13,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/players", (req, res) => {
+  res.send(playerData);
 });
 
 app.listen(port, () => {
